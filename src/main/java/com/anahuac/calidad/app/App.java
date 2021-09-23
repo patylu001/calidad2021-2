e3731ec9db7b14862dc0e3fb0285bfc21b18e9e9
Patricia Ortegon <patricia.ortegon@gmail.com>
2021-09-23 18:38:57 -0500

    Modificando desde eclipse

--- src/main/java/com/anahuac/calidad/app/App.java
+++ src/main/java/com/anahuac/calidad/app/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World desde el campus! NO hagan esto!!!" );
+        System.out.println( "Hello World desde el campus!Modificacion desde eclipse!!!" );
         System.out.println( "Hello World desde el campus! Esto si lo pueden hacer, editar localmente" );
     }
 }
