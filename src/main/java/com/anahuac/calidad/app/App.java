e4d6f79dab0132cda55e105c58042ec17497d153
patylu001 <patricia.ortegon@gmail.com>
2021-09-23 18:18:38 -0500

    No hacer esto

--- src/main/java/com/anahuac/calidad/app/App.java
+++ src/main/java/com/anahuac/calidad/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World desde el campus!" );
+        System.out.println( "Hello World desde el campus! NO hagan esto!!!" );
     }
 }
